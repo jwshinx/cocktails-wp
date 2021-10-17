dbc33729783895b5b53b7c89ee0390c370382c4c
joel shin <jwshin@gmail.com>
2021-10-17 15:34:54 -0700

    installed and wired up prettier and setup *yarn format*

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import { Counter } from './components/Counter'
 export const App = () => {
   return (
     <>
-      <h1>Edited Again 2 React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
+      <h1>
+        Edited Again 2 React TypeScript Webpack Starter Template -{' '}
+        {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <img src={IMAGE} alt="Golden State Warriors" width="200" />
       <img src={LOGO} alt="Golden State Warriors" width="200" />
       <Counter />
