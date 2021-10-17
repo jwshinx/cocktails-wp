f5ad6822e94d61a0ff4361051b566e7ed303f9c3
joel shin <jwshin@gmail.com>
2021-10-17 09:50:47 -0700

    modified webpack config to handle dev and prod envs

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import LOGO from './logo.svg'
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
       <img src={IMAGE} alt="Golden State Warriors" width="200" />
       <img src={LOGO} alt="Golden State Warriors" width="200" />
     </>
