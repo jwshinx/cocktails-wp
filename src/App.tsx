10ce09897eb734e140d261f5ddbf136b344bc403
joel shin <jwshin@gmail.com>
2021-10-17 11:10:41 -0700

    installed react-refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './styles.css'
 import IMAGE from './gsw.png'
 import LOGO from './logo.svg'
+import { Counter } from './components/Counter'
 
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
+      <h1>Edited Again 2 React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
       <img src={IMAGE} alt="Golden State Warriors" width="200" />
       <img src={LOGO} alt="Golden State Warriors" width="200" />
+      <Counter />
     </>
   )
 }
