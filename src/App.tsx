4c2f7b80cbbb775d1bda58fd666cd6f7cd54a31f
joel shin <jwshin@gmail.com>
2021-10-17 01:12:56 -0700

    created declarations for png, svg; configged webpack to know png and svg types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import './styles.css'
+import IMAGE from './gsw.png'
+import LOGO from './logo.svg'
+
 export const App = () => {
   return (
     <>
       <h1>React TypeScript Webpack Starter Template</h1>
+      <img src={IMAGE} alt="Golden State Warriors" width="200" />
+      <img src={LOGO} alt="Golden State Warriors" width="200" />
     </>
   )
 }
