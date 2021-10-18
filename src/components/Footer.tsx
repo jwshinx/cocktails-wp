b79ca3fbf8dd70c784179bd84df85c4e0e87dcea
joel shin <jwshin@gmail.com>
2021-10-17 18:56:04 -0700

    yarn add bootstrap; works webpack transpired in build also

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,96 @@ export default function Footer() {
   return (
     <div className="footer">
       <h3>Footer</h3>
+      <br />
+      <button className="btn btn-primary">Ooo</button>
+      <br />
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              Accordion Item #1
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>This is the first item of accordion body.</strong> It is
+              shown by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. It is also worth noting
+              that just about any HTML can go within the{' '}
+              <code>.accordion-body</code>, though the transition does limit
+              overflow.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              Accordion Item #2
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>This is the second</strong> It is hidden by default, until
+              the collapse plugin adds the appropriate classes that we use to
+              style each element. It is also worth noting that just about any
+              HTML can go within the <code>.accordion-body</code>, though the
+              transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              Accordion Item #3
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>This is the third.</strong> It is hidden by default, until
+              the collapse plugin adds the appropriate classes that we use to
+              style each element. It is also worth noting that just about any
+              HTML can go within the <code>.accordion-body</code>, though the
+              transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
