242b1c20fcd9736dfa70ed48733fba4a227a800e
joel shin <jwshin@gmail.com>
2021-10-17 18:35:55 -0700

    played with footer and css

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import './styles.css'
 import IMAGE from './gsw.png'
 import LOGO from './logo.svg'
 import { Counter } from './components/Counter'
+import Footer from './components/Footer'
 
 export const App = () => {
   return (
     <>
       <h1>
-        Edited 3 React TypeScript Webpack Starter Template -{' '}
+        Edited 4 React TypeScript Webpack Starter Template -{' '}
         {process.env.NODE_ENV} {process.env.name} {process.env.city}{' '}
         {process.env.COLOR}
       </h1>
       <img src={IMAGE} alt="Golden State Warriors" width="200" />
       <img src={LOGO} alt="Golden State Warriors" width="200" />
       <Counter />
+      <Footer />
     </>
   )
 }
