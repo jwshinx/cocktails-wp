e894eb3a6434305c918ccbab785d5a90a60a74e3
joel shin <jwshin@gmail.com>
2021-10-18 00:19:49 -0700

    installed react-router dom; css module; boilerplate bootstrap layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './styles.css'
@@ -8,16 +9,165 @@ import Footer from './components/Footer'
 
 export const App = () => {
   return (
-    <>
-      <h1>
-        Edited 4 React TypeScript Webpack Starter Template -{' '}
-        {process.env.NODE_ENV} {process.env.name} {process.env.city}{' '}
-        {process.env.COLOR}
-      </h1>
-      <img src={IMAGE} alt="Golden State Warriors" width="200" />
-      <img src={LOGO} alt="Golden State Warriors" width="200" />
-      <Counter />
-      <Footer />
-    </>
+    <Router>
+      <div className="container">
+        <div className="row">
+          <div className="col-3 sidenav">
+            <h2>Logo</h2>
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/age">Age</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/gender">Gender</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/geo">Geo</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="col-9">
+            <div className="row mt-3 mb-3">
+              <h3>
+                {process.env.NODE_ENV} {process.env.name} {process.env.city}{' '}
+                {process.env.COLOR}
+              </h3>
+              <div className="col-3">
+                <img src={IMAGE} alt="Golden State Warriors" width="200" />
+              </div>
+
+              <div className="col-3">
+                <img src={LOGO} alt="Golden State Warriors" width="200" />
+              </div>
+
+              <div className="col-3">
+                <Counter />
+              </div>
+              <div className="col-3">
+                <div className="well orange">
+                  <h4>Dashboard</h4>
+                </div>
+              </div>
+            </div>
+
+            <div className="row mt-3 mb-3">
+              <div className="col-3">
+                <div className="card card-body bg-light">
+                  <h4>Users</h4>
+                </div>
+              </div>
+              <div className="col-3">
+                <div className="card card-body bg-light">
+                  <p>ketchupp</p>
+                </div>
+              </div>
+              <div className="col-3">
+                <div className="card card-body bg-faded">
+                  <p>lettuce</p>
+                </div>
+              </div>
+              <div className="col-3">
+                <div className="card card-body bg-info">
+                  <h4>Bounce</h4>
+                  <p>30%</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-4">
+                <div className="card card-body bg-faded">
+                  <p>Text</p>
+                  <p>Text</p>
+                  <p>Text</p>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="card card-body bg-warning">
+                  <p>Text</p>
+                  <p>Text</p>
+                  <p>Text</p>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="card card-body bg-secondary">
+                  <p className="text-danger">Text</p>
+                  <p>Text</p>
+                  <p>Text</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="row mt-3 mb-3">
+              <Switch>
+                <Route path="/age">
+                  <div className="col-12">
+                    <div className="card card-body">
+                      <AgeComponent />
+                    </div>
+                  </div>
+                </Route>
+                <Route path="/gender">
+                  <div className="col-12">
+                    <div className="card card-body">
+                      <GenderComponent />
+                    </div>
+                  </div>
+                </Route>
+                <Route path="/geo">
+                  <div className="col-12">
+                    <div className="card card-body">
+                      <GeoComponent />
+                    </div>
+                  </div>
+                </Route>
+                <Route path="/">
+                  <div className="col-12">
+                    <div className="card card-body">
+                      <HomeComponent />
+                    </div>
+                  </div>
+                </Route>
+              </Switch>
+            </div>
+
+            <div className="row">
+              <div className="col-8">
+                <div className="card card-body bg-danger">
+                  <Footer />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="card card-body bg-success">
+                  <p>Text</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   )
+}
+
+function HomeComponent() {
+  return <h4>Home</h4>
+}
+
+function GeoComponent() {
+  return <h4>Geo component</h4>
+}
+
+function AgeComponent() {
+  return <h4>Age component</h4>
+}
+
+function GenderComponent() {
+  return <h4>Gender component</h4>
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 
+import styles from './Counter.module.css'
+
 export const Counter = () => {
   const [count, setCount] = useState(0)
   return (
-    <div>
+    <div className={`counter ${styles.dark}`}>
       <h3>Update the count and edit src/App.tsx, state is preserved</h3>
-      <button onClick={() => setCount((c) => c + 1)}>Count - {count}</button>
+      <button
+        className="btn btn-primary"
+        onClick={() => setCount((c) => c + 1)}
+      >
+        Count - {count}
+      </button>
     </div>
   )
 }
