a538cb0aa3d10163ba79427cdd184a42c1385b1e
joel shin <jwshin@gmail.com>
2021-10-22 20:39:15 -0700

    added test but fails because of jest-dom; need to install variant

--- src/components/title/__test__/Title.test.js
+++ src/components/title/__test__/Title.test.js
@@ -4,9 +4,16 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { render } from '@testing-library/react'
+import 'jest-dom/extend-expect'
 import Title from '../Title'
 
-it('renders properly', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Title text="Foobar" />, div)
+  ReactDOM.render(<Title></Title>, div)
+})
+
+it('renders title properly', () => {
+  const { getByTestId } = render(<Title text="Foobar"></Title>)
+  expect(getByTestId('title')).toHaveTextContent('Foobar')
 })
